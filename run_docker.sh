808c143cc0b40978a6da70a1041cb20ffcedade1
NicoleFormenti <59973282+nikiiny@users.noreply.github.com>
2021-07-01 18:46:26 +0200

    uploading docker

--- run_docker.sh
+++ run_docker.sh
@@ -1,2 +1,2 @@
-docker build --file Dockerfile --tag inforet-docker . 
-docker run -p 8888:8888 inforet-docker
+docker build --file Dockerfile -t inforet-docker . 
+docker run -p 8888:8888 --name inforet-docker inforet-docker
